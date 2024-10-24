8a324c7457ff15507ce251ed5e98910d583020bb
ccute <80099327+Kostiskat@users.noreply.github.com>
2024-10-25 01:48:12 +0300

    test commit

--- src/main/kotlin/com/redstonelabs/react/Main.kt
+++ src/main/kotlin/com/redstonelabs/react/Main.kt
@@ -1,11 +1,13 @@
 package com.redstonelabs.react
 
+import org.bukkit.Bukkit
 import org.bukkit.plugin.java.JavaPlugin
 
 class Main : JavaPlugin() {
 
     override fun onEnable() {
         // Plugin startup logic
+        logger.info("This is a test message! :D")
     }
 
     override fun onDisable() {
